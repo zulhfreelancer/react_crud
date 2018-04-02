ebeca08e56eb651fd0f2c30efb0755050d692587
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-04-02 15:36:44 +0000

    Index - load all products from Local Storage

--- src/App.js
+++ src/App.js
@@ -2,17 +2,51 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var products = [
+  {
+    name: 'iPad',
+    price: 200
+  },
+  {
+    name: 'iPhone',
+    price: 650
+  }
+];
+
+localStorage.setItem("products", JSON.stringify(products));
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: []
+    };
+  }
+
+  componentWillMount() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    var products = JSON.parse( localStorage.getItem("products") );
+    this.setState({ products });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Products Manager</h1>
+
+        {
+          this.state.products.map(product => {
+            return (
+              <div key={product.name}>
+                <span>{product.name}</span> <span>{product.price}</span>
+              </div>
+            )
+          })
+        }
       </div>
     );
   }
